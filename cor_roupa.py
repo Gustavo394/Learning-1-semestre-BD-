f9970ee6b8e91791d3a6fb15899b0f8b721d854f
Gustavo Henrique Silva <111407920+Gustavo394@users.noreply.github.com>
2022-09-02 16:48:11 -0300

    Error correction

--- cor_roupa.py
+++ cor_roupa.py
@@ -1,19 +1,17 @@
-from fileinput import close
-from multiprocessing.resource_sharer import stop
 from PySimpleGUI import PySimpleGUI as sg
 import datetime
 
-sg.theme('Reddit')
+sg.theme('Topanga')
 
 while True:
     try:
         inicio = [
-            [sg.Text('Infoeme seu nome'), sg.Input(key='nome', size=(20, 1))],
+            [sg.Text('Informe seu nome'), sg.Input(key='nome', size=(20, 1))],
             [sg.Button('Próximo')]
         ]
         janela = sg.Window('Cor de roupa', inicio)
         eventos, valores = janela.read()
-        
+
         if eventos == sg.WINDOW_CLOSED:
             break
 
@@ -22,12 +20,12 @@ while True:
             nome = valores['nome']
             instrucoes = [
                 [sg.Text(f'Olá {nome} é um prazer conhecer você!')],
-                [sg.Text('Vamos realizar um procedimento para saber se você está vestindo roupas de\numa única cor, nenhuma roupa de uma cor,\ncores que não se repetem ou se está vestindo cores igualmente distribúidas.')],
+                [sg.Text('Vamos realizar um procedimento para saber se você está vestindo roupas de uma única cor, nenhuma roupa de uma certa cor, cores que não se repetem ou se está vestindo cores igualmente distribuídas.', size=(60, 3))],
                 [sg.Text('Escolha uma das opções:')],
-                [sg.Button('Estou vestindo somente roupas de uma única cor', size=(14, 5)),
-                sg.Button('Não estou usando nenhuma roupa de uma certa cor', size=(14, 5)),
-                sg.Button('Estou vestindo roupas de cores que não se repetem', size=(14, 5)), 
-                sg.Button('Estou vestindo roupas de cores igualmente distribuídas', size=(14, 5))],
+                [sg.Button('Estou vestindo somente roupas de uma única cor', size=(14, 5), key='1'),
+                sg.Button('Não estou usando nenhuma roupa de uma certa cor', size=(14, 5), key='2'),
+                sg.Button('Estou vestindo roupas de cores que não se repetem', size=(14, 5), key='3'), 
+                sg.Button('Estou vestindo roupas de cores igualmente distribuídas', size=(14, 5), key='4')],
             ]
             janela = sg.Window('Instruções', instrucoes)
             eventos, valores = janela.read()
@@ -38,6 +36,7 @@ while True:
                 break
 
             condicao = True
+            com_info = True
 
             cor = [
                 [sg.Text('Selecione a cor da roupa:')],
@@ -49,7 +48,7 @@ while True:
                 [sg.Text('Calça'), sg.Combo(['Preto', 'Branco', 'Amarelo', 'Azul', 'Verde', 'Vermelho'])],
                 [sg.Text('Meia'), sg.Combo(['Preto', 'Branco', 'Amarelo', 'Azul', 'Verde', 'Vermelho'])],
                 [sg.Text('Sapato'), sg.Combo(['Preto', 'Branco', 'Amarelo', 'Azul', 'Verde', 'Vermelho'])],
-                [sg.Button('Confirmar')]
+                [sg.Button('Confirmar')],
             ]
             falso = [
                 [sg.Text('A afirmação é falsa!')]
@@ -57,8 +56,11 @@ while True:
             verdadeiro = [
                 [sg.Text('A afirmação é verdadeira!')]
             ]
-            
-            if (info == 'Estou vestindo somente roupas de uma única cor') or (info == 'Não estou usando nenhuma roupa de uma certa cor'):
+            vazio = [
+                [sg.Text('Falta informação!')]
+            ]
+
+            if (info == '1') or (info == '2'):
                 janela = sg.Window('Cor', cor)
                 eventos, valores = janela.read()
                 cor = eventos
@@ -68,35 +70,34 @@ while True:
                 break
 
             janela = sg.Window('Peça', peca)
-            janela.read()
             eventos, valores = janela.read()
-            janela.close()
             peca = valores
+            janela.close()
 
             if eventos == sg.WINDOW_CLOSED:
                 break                
 
             if (eventos == 'Confirmar'):
-                if (info == 'Estou vestindo somente roupas de uma única cor'):
+                if (valores[0] == '') or (valores[1] == '') or (valores[2] == '') or (valores[3] == ''):
+                    print(valores)
+                    com_info = False
+                elif (info == '1'):
                     for linha in range(4):
                         if peca[linha] != cor:
                             print(peca[linha])
-                            condicao = False
-                            break
-                elif (info == 'Não estou usando nenhuma roupa de uma certa cor'):
+                            condicao = False                            
+                elif (info == '2'):
                     for linha in range(4):
                         if peca[linha] == cor:
                             condicao = False
-                            break
-                elif (info == 'Estou vestindo roupas de cores que não se repetem'):
+                elif (info == '3'):
                     cont = 1
                     for linha1 in range(3):
                         for linha2 in range(cont, 3):
                             if peca[linha1] == peca[linha2]:
                                 condicao = False
-                                break
                         cont = cont + 1
-                if (info == 'Estou vestindo roupas de cores igualmente distribuídas'):
+                if (info == '4'):
                     if peca[0] == peca[1] == peca[2] == peca[3]:
                         condicao = False
                     else:
@@ -112,13 +113,15 @@ while True:
                                         a = a + 1
                                     elif b == 1:
                                         condicao = False
-                                        break
                                     elif a == 1:
                                         b = b + 1
                             cont = cont + 1
                         if (a != b):
                             condicao = False
-            
+            if com_info == False:
+                janela = sg.Window('Fim', vazio)
+                janela.read()
+                break
             if condicao == False:
                 janela = sg.Window('Fim', falso)
                 janela.read()
@@ -127,7 +130,7 @@ while True:
                 janela = sg.Window('Fim', verdadeiro)
                 janela.read()
                 break
-        
+
     except Exception as erro:
         date = str(datetime.date.today())
         register_error = [erro.__class__]
@@ -135,4 +138,5 @@ while True:
             for linha in register_error:
                 error.write(str(date) + ' - ' + str(register_error) + '\n')
                 error.seek(0)
-
+                error.close()
+                
