2dd099aab9715835e235702748dfc5386eb06827
Gustavo Henrique Silva <gustavo.silva394@fatec.sp.gov.br>
2022-09-21 02:30:39 -0300

    Learning

--- 6 - poluicao_JS/aviso.js
+++ 6 - poluicao_JS/aviso.js
@@ -1,16 +1,16 @@
 //var decimal = poluicao = prompt('Informe o índice de poluição: ')
 function calcularpoluicao(){
     let poluicao = document.getElementById("pol").value;
-    if (poluicao >= 0.55){
+    if (poluicao >= 55){
         window.alert("Saudações, por motivos de seguração ao meio ambiente, solicitamos que todas as atividades de todas as empresas sejam suspensas imediatamente!")
     }
-    else if (poluicao >= 0.45){
+    else if (poluicao >= 45){
         window.alert("Saudações, por motivos de seguração ao meio ambiente, solicitamos que todas as atividades das empresas dos grupos 1 e 2 sejam suspensas imediatamente!")
     }
-    else if (poluicao >= 0.3){
+    else if (poluicao >= 30){
         window.alert("Saudações, por motivos de seguração ao meio ambiente solicitamos que todas as atividades das empresas do grupo 1 sejam suspensas imediatamente!")
     }
-    else if (poluicao >= 0.27){
+    else if (poluicao >= 27){
         window.alert("Índice de poluição elevado, caso necessário as empresas serão notificadas para suspender as atividades!")
     }
     else{
