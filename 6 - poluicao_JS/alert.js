689e44cd5edb3660fd80e339d65c0531f1fe936a
Gustavo Henrique Silva <gustavo.silva394@fatec.sp.gov.br>
2022-09-21 02:02:01 -0300

    Learning

--- 6 - poluicao_JS/alert.js
+++ 6 - poluicao_JS/alert.js
@@ -1,17 +1,19 @@
-var decimal = poluicao = prompt('Informe o índice de poluição: ')
-
-if (poluicao >= 0.55){
-    window.alert("Saudações, por motivos de seguração ao meio ambiente, solicitamos que todas as atividades de todas as empresas sejam suspensas imediatamente!")
-}
-else if (poluicao >= 0.45){
-    window.alert("Saudações, por motivos de seguração ao meio ambiente, solicitamos que todas as atividades das empresas dos grupos 1 e 2 sejam suspensas imediatamente!")
-}
-else if (poluicao >= 0.3){
-    window.alert("Saudações, por motivos de seguração ao meio ambiente solicitamos que todas as atividades das empresas do grupo 1 sejam suspensas imediatamente!")
-}
-else if (poluicao >= 0.27){
-    window.alert("Índice de poluição elevado, caso necessário as empresas serão notificadas para suspender as atividades!")
-}
-else{
-    window.alert("O índice de poluição se encontra dentro dos padroes!")
+//var decimal = poluicao = prompt('Informe o índice de poluição: ')
+function calcularpoluicao(){
+    let poluicao = document.getElementById("pol").value;
+    if (poluicao >= 0.55){
+        window.alert("Saudações, por motivos de seguração ao meio ambiente, solicitamos que todas as atividades de todas as empresas sejam suspensas imediatamente!")
+    }
+    else if (poluicao >= 0.45){
+        window.alert("Saudações, por motivos de seguração ao meio ambiente, solicitamos que todas as atividades das empresas dos grupos 1 e 2 sejam suspensas imediatamente!")
+    }
+    else if (poluicao >= 0.3){
+        window.alert("Saudações, por motivos de seguração ao meio ambiente solicitamos que todas as atividades das empresas do grupo 1 sejam suspensas imediatamente!")
+    }
+    else if (poluicao >= 0.27){
+        window.alert("Índice de poluição elevado, caso necessário as empresas serão notificadas para suspender as atividades!")
+    }
+    else{
+        window.alert("O índice de poluição se encontra dentro dos padroes!")
+    }
 }
